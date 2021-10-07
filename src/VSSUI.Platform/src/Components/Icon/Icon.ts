81963fdd8a1d8f9a899789c5722a1834275d9589
XiaoYang <tlylz99@live.cn>
2021-10-07 17:49:38 +0800

    Add icon files generation.

---
+++ src/VSSUI.Platform/src/Components/Icon/Icon.ts
@@ -0,0 +1,13 @@
+import { IIconProps } from "azure-devops-ui/Components/Icon/Icon.Props";
+import { Icon as IconV1 } from "azure-devops-ui/Components/Icon/Icon";
+import { css } from "azure-devops-ui/Util";
+import { getIconNameMap } from "./IconNames";
+
+export { IIconProps };
+
+export function Icon(props: Readonly<IIconProps>): JSX.Element {
+    return IconV1({
+        className: css(props.className, 'ms-IconSet--' + getIconNameMap(props.iconName)),
+        ...props
+    });
+}

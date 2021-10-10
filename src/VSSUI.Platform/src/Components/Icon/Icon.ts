dfe7eb639916ce50a246c0da965105115bb89f2e
XiaoYang <tlylz99@live.cn>
2021-10-10 16:56:46 +0800

    Use css instead of scss.

--- src/VSSUI.Platform/src/Components/Icon/Icon.ts
+++ src/VSSUI.Platform/src/Components/Icon/Icon.ts
@@ -1,9 +1,11 @@
 import { IIconProps } from "azure-devops-ui/Components/Icon/Icon.Props";
 import { Icon as IconV1 } from "azure-devops-ui/Components/Icon/Icon";
 import { css } from "azure-devops-ui/Util";
-import { getIconNameMap } from "./IconNames";
 
-export { IIconProps };
+import { getIconNameMap } from "./IconNames";
+import "./ExtendedIcon.css";
+
+export type { IIconProps };
 
 export function Icon(props: Readonly<IIconProps>): JSX.Element {
     return IconV1({
